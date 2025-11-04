4f054b26345c302cedf6a49859095445ef6a0d97
Thiago Nogueira <131370555+thiagond-code@users.noreply.github.com>
2025-11-04 12:09:32 +0000

    chore(setup): configure project tooling

--- code/eslint.config.mjs
+++ code/eslint.config.mjs
@@ -5,6 +5,7 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
